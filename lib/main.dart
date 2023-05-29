6104e38237cff4d6971e028bfd85d72108c7cb13
Lans12345 <olanalans12345@gmail.com>
2023-05-29 10:31:24 +0800

    added auto login

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:jeepney/screens/auth/login_page.dart';
+import 'package:jeepney/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +13,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: LoginScreen(),
+      home: StreamBuilder<User?>(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return const HomeScreen();
+            } else {
+              return LoginScreen();
+            }
+          }),
     );
   }
 }
